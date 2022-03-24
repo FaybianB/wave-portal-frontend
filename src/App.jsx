76e62fd1aeed6b375660f737924536b490a8855c
UGonGetThisWord <48360446+FaybianB@users.noreply.github.com>
2022-03-24 21:00:56 +0000

    Update the contract

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import abi from "./utils/WavePortal.json";
 
 export default function App() {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [message, setMessage] = useState("");
 	const [totalWaves, setTotalWaves] = useState(0);
+	const [allWaves, setAllWaves] = useState([]);
 	const [isProcessingTransaction, setIsProcessingTransaction] = useState(false);
-  const contractAddress = "0x586C3019ab61c8D8fFE2f3E9db991B99BfB7022f";
+  const contractAddress = "0x225f20BC3ABe60A385C6577701F6F2B6f6953435";
   const contractABI = abi.abi;
 	
 	const checkIfWalletIsConnected = async () => {
@@ -35,14 +37,15 @@ export default function App() {
 				setCurrentAccount(account);
 
 				getTotalWaves();
+				getAllWaves();
       } else {
         console.log("No authorized account found")
-      }
+			}
     } catch (error) {
       console.log(error);
     }
 	}
-
+	
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -59,12 +62,15 @@ export default function App() {
 			setCurrentAccount(accounts[0]);
 			
 			getTotalWaves();
+			getAllWaves();
     } catch (error) {
       console.log(error)
     }
   }
 	
 	const wave = async () => {
+		event.preventDefault();
+		
 		setIsProcessingTransaction(true);
 		
     try {
@@ -76,13 +82,15 @@ export default function App() {
 				/*
         * Execute the actual wave from your smart contract
         */
-        const waveTxn = await wavePortalContract.wave();
+        const waveTxn = await wavePortalContract.wave(message, { gasLimit: 400000 });
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
         console.log("Mined -- ", waveTxn.hash);
-				
+
+				setMessage('');
 				getTotalWaves();
+				getAllWaves();
       } else {
         console.log("Ethereum object doesn't exist!");
       }
@@ -112,6 +120,36 @@ export default function App() {
     }
 	}
 
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+				const wavePortalContract = getContract(ethereum);
+        const waves = await wavePortalContract.getAllWaves();
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+	      let wavesCleaned = waves.map(wave => {
+	        return {
+	          address: wave.waver,
+	          timestamp: new Date(wave.timestamp * 1000),
+	          message: wave.message,
+	        };
+	      });
+				// Flip the waves array so that the most recent message appears first
+				wavesCleaned = wavesCleaned.reverse();
+
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+	
 	const getContract = (ethereum) => {
 		const provider = new ethers.providers.Web3Provider(ethereum);
 		const signer = provider.getSigner();
@@ -119,12 +157,52 @@ export default function App() {
 
 		return wavePortalContract;
  	}
+
+	const handleAccountChange = async () => {
+			const accounts = await ethereum.request({ method: "eth_accounts" });
+			
+			if (accounts.length === 0) {
+				console.log('wallet disconnected');
+				setCurrentAccount('');
+				setAllWaves([]);
+				setTotalWaves(0);
+			} else {
+        const account = accounts[0];
+        
+				console.log("Found an authorized account:", account);
+   
+				setCurrentAccount(account);
+			}
+	}
+	
+	const attachWalletListeners = () => {
+    const { ethereum } = window;
+		const wavePortalContract = getContract(ethereum);
+		
+		ethereum.on('accountsChanged', handleAccountChange);
+		
+		const onNewWave = (from, timestamp, message) => {
+	    console.log("NewWave", from, timestamp, message);
+	    
+			setAllWaves(prevState => [
+	      ...prevState,
+	      {
+	        address: from,
+	        timestamp: new Date(timestamp * 1000),
+	        message: message,
+	      },
+	    ]);
+	  };
+		
+		wavePortalContract.on("NewWave", onNewWave);
+	}
 	
   /*
   * This runs our function when the page loads.
   */
   useEffect(async () => {
   	checkIfWalletIsConnected();
+		attachWalletListeners();
   }, []);
 	
   return (
@@ -139,13 +217,29 @@ export default function App() {
 				{
 					currentAccount ? 
 						<>
-			        <button
-								disabled={isProcessingTransaction}
-								className={isProcessingTransaction ? "waveButtonProcessing" : "waveButton"}
-								onClick={wave}
-							>
-								{isProcessingTransaction ? 'Processing transaction...' : 'Wave at Me'}
-			        </button>
+							<form onSubmit={wave}>
+				        <label>
+				          Enter a message:
+				          <textarea
+										value={message}
+										onChange={(event) => setMessage(event.target.value)}
+									/>
+				        </label>
+				        <input
+									disabled={isProcessingTransaction || !message}
+									className={
+										isProcessingTransaction
+										? "waveButtonProcessing"
+										: "waveButton"
+									}
+									type="submit"
+									value={
+										isProcessingTransaction
+										? 'Processing transaction...'
+										: 'Wave at Me'
+									}
+								/>
+				      </form>
 							<span className="waveCounter">Total Number of Waves: {totalWaves}</span>
 						</>
 					:
@@ -157,6 +251,33 @@ export default function App() {
 	            Connect Wallet
 	          </button>
 					</>
+				}
+				{
+					currentAccount && allWaves.length > 0 &&
+					(		
+						<table className="waveTable">
+							<thead>
+								<tr>
+									<th>Address</th>
+									<th>Time</th>
+									<th>Message</th>
+								</tr>
+							</thead>
+							{
+								allWaves.map((wave, index) => {
+									return (
+										<tbody key={index}>
+											<tr>
+												<td>{wave.address}</td>
+												<td>{wave.timestamp.toString()}</td>
+												<td>{wave.message}</td>
+											</tr>
+										</tbody>
+									)
+								})
+							}
+						</table>
+					)
 				}
       </div>
     </div>
